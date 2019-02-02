8c4a9ae19906c0f7ff3db1861fb0e521fe5f8a5d
Samson Badlia <sbadlia00@stuy.edu>
2019-02-01 19:32:12 -0500

    Basic test cases

---
+++ driver.java
@@ -0,0 +1,14 @@
+public class driver{
+
+  public static void main(String[] args) {
+  System.out.println(recursion.sqrt(100.0));
+  System.out.println("Should be 10");
+  System.out.println(recursion.sqrt(64.0));
+  System.out.println("Should be 8");
+  //System.out.println(recursion.sqrt(-100.00));
+  //System.out.println("Should be error");
+  System.out.println(recursion.sqrt(2.0));
+  System.out.println("Should be 1.4 and then some");
+  }
+
+}
